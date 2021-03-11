9b17e7fc02fa029dce0ea81d33b42af3395e2775
Claudio Urrutia <claudio.urrutia@usach.cl>
2021-03-11 03:11:13 -0300

    Final Funcional

--- djangoWeather/api/consumer.py
+++ djangoWeather/api/consumer.py
@@ -1,5 +1,7 @@
 # Built in imports.
 import json
+import datetime
+import threading
 # Third Party imports.
 from channels.exceptions import DenyConnection
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -7,21 +9,22 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import AnonymousUser
 from .views import get_weather
-class weatherConsumer(AsyncWebsocketConsumer):
+class WeatherConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        print("Conected")
-        await self.send(text_data=json.dumps(get_weather("params")))
+        print("Connected")
+        await self.send_json()
         print("Sended")
+  
 
     async def disconnect(self, close_code):
+        print("Disconnected")
         pass
 
-
-    async def celery_message(self, event):
-        print("Service Received")
-        print(event)
-        await self.send({
-            "type": "websocket.send",
-            "text": event["text"],
-        })
+    async def send_json(self):
+        """
+        Encode the given content as JSON and send it to the client.
+        """
+        await self.send(text_data=json.dumps(get_weather("params")))
+        
+        
--- djangoWeather/api/routing.py
+++ djangoWeather/api/routing.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from .consumer import weatherConsumer
-websockets = URLRouter([
+from .consumer import WeatherConsumer
+
+ws_urlpatterns=[
+
     path(
-        "ws/weather", weatherConsumer,
-        name="weather",
+        "ws/weather", WeatherConsumer, name="weather"
     ),
-])
+
+]
+
+
+
